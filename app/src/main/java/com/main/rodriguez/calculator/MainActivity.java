10dd2cb2e62db480a8a4d90bd45b1eaa4a1dd772
noelrodriguez <noel.rodz17@gmail.com>
2016-10-24 16:23:08 -0700

    Intro Code

---
+++ app/src/main/java/com/main/rodriguez/calculator/MainActivity.java
@@ -0,0 +1,53 @@
+package com.main.rodriguez.calculator;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void clearInput(View view) {
+        TextView userInput = (TextView)findViewById(R.id.textView1);
+        Button clear = (Button)findViewById(R.id.button11);
+        if (userInput.toString() != null) {
+            userInput.setText("0");
+            clear.setText("AC");
+        }
+    }
+}
